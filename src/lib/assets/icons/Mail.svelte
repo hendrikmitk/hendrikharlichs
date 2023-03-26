c10771588fff2217905b4322b61f568fcd260f2d
hendrikmitk <hharlichs@googlemail.com>
2023-03-26 22:11:47 +0200

    Add social icons for footer

---
+++ src/lib/assets/icons/Mail.svelte
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	<path
+		fill="currentColor"
+		d="M20 4H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm-.4 4.3-6.5 4a2 2 0 0 1-2.2 0l-6.5-4a.8.8 0 1 1 .9-1.5L12 11l6.7-4.2a.8.8 0 1 1 .9 1.4z"
+	/>
+</svg>

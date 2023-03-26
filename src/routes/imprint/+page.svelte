ad40605723e1bbf90c5bb37e3d9f28a20562bba4
hendrikmitk <hharlichs@googlemail.com>
2023-03-26 22:06:17 +0200

    Add placeholder imprint

---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,21 @@
+<h1>Impressum</h1>
+
+<p>Inhaltlich verantwortlich gemäß § 5 TMG:</p>
+
+<p>
+	Max Mustermann<br />Musterstraße 1<br />23456 Musterstadt
+</p>
+
+<h2>Kontakt</h2>
+
+Telefon: +49 (0) 123 456 789 01 E-Mail: info@maxmustermann.de
+
+<h2>Haftungshinweis</h2>
+
+<p>
+	Trotz sorgfältiger inhaltlicher Kontrolle übernehme ich keine Haftung für die Inhalte externer
+	Links.<br />Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber
+	verantwortlich.
+</p>
+
+<p>Note: This page is in German to conform with the imprint requirements of German law.</p>

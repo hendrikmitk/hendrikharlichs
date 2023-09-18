7541ed365097e3916aff390e64ee1e295eba74fe
hendrikmitk <hendrikharlichs@fastmail.com>
2023-09-18 13:48:34 +0200

    refactor: Make implementation less experimental
    The animation-timeline CSS property is experimental technology and is not yet supported by all major browsers.

--- src/routes/notes/[slug]/+layout.svelte
+++ src/routes/notes/[slug]/+layout.svelte
@@ -1,56 +1,45 @@
 <script lang="ts">
 	const scrollThreshold = 82;
 	let isHeaderInView: boolean;
+	let scrollProgress: number;
 
 	const parseScroll = (): void => {
 		isHeaderInView = window.scrollY < scrollThreshold;
 	};
+
+	const updatescrollProgress = (): void => {
+		let scroll = document.documentElement.scrollTop;
+		let height =
+			document.documentElement.scrollHeight -
+			document.documentElement.clientHeight;
+		scrollProgress = +((scroll / height) * 100).toFixed(2);
+	};
 </script>
 
-<svelte:window on:scroll={parseScroll} />
+<svelte:window on:scroll={parseScroll} on:scroll={updatescrollProgress} />
 
-<div class:tail={!isHeaderInView} />
+<div style:width={scrollProgress + '%'}>
+	{#if !isHeaderInView}
+		<div style:width={100 - scrollProgress + '%'} />
+	{/if}
+</div>
 
 <slot />
 
 <style lang="scss">
 	@include for(tablet-and-up) {
-		@keyframes grow {
-			to {
-				width: 100%;
-			}
-		}
-
-		@keyframes shrink {
-			to {
-				width: 0;
-			}
-		}
-
 		div {
-			animation: grow linear;
-			animation-timeline: scroll(root);
+			height: 0.375rem;
 			background-color: var(--color--page-accent);
-			height: 0.25rem;
 			inset: 0 auto auto 0;
 			position: fixed;
-			width: 0;
 
-			&::after {
-				animation: shrink linear;
-				animation-timeline: scroll(root);
+			> div {
 				background-color: transparent;
+				background-color: var(--color--header-background);
 				content: '';
-				height: 0.25rem;
 				inset: 0 0 auto auto;
 				position: inherit;
-				width: 100%;
-			}
-
-			&.tail {
-				&::after {
-					background-color: var(--color--header-background);
-				}
 			}
 		}
 	}

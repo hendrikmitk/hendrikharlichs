cf66c87f5406de3b3d43dba0dc8ff3fc128cead2
hendrikmitk <hendrikharlichs@fastmail.com>
2023-09-26 18:14:52 +0200

    feat: Display reading time in blog post

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,4 @@
+import capitalizeFirstLetter from './capitalizeFirstLetter';
 import closeMenuIfOpen from './closeMenuIfOpen';
 import formatDate from './formatDate';
 import getPosts from './getPosts';
@@ -7,6 +8,7 @@ import toggleScrollLock from './toggleScrollLock';
 import trimFrontMatter from './trimFrontMatter';
 
 export {
+	capitalizeFirstLetter,
 	closeMenuIfOpen,
 	formatDate,
 	getPosts,

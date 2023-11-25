d1dff44bfa164d52f4a59369fa5e83cdf0d0b977
hendrikmitk <hendrikharlichs@fastmail.com>
2023-11-25 12:01:58 +0100

    feat: Introduce wireframe component

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,26 +1,7 @@
 <script>
-	import Footer from '$lib/components/Footer.svelte';
-	import Header from '$lib/components/Header.svelte';
-
-	import '$lib/scss/global.scss';
+	import Wireframe from '$lib/components/Wireframe.svelte';
 </script>
 
-<Header />
-
-<main>
+<Wireframe>
 	<slot />
-</main>
-
-<Footer />
-
-<style lang="scss">
-	main {
-		margin: 2rem 1rem;
-
-		@include for(tablet-and-up) {
-			margin: 4rem auto 2rem;
-			width: 100%;
-			max-width: 46rem;
-		}
-	}
-</style>
+</Wireframe>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,5 @@
 <script>
-	import Footer from '$lib/components/Footer.svelte';
-	import Header from '$lib/components/Header.svelte';
+	import Wireframe from '$lib/components/Wireframe.svelte';
 
 	import { siteTitle } from '$lib/config';
 	import { description } from '$lib/data';
@@ -16,24 +15,8 @@
 	<meta name="twitter:description" content={description} />
 </svelte:head>
 
-<Header />
-
-<main>
+<Wireframe>
 	<h1>Page Not Found</h1>
 	<p>Sorry, the page you are looking for does not exist.</p>
 	<p>Go back to the <a href="/">home page</a> instead.</p>
-</main>
-
-<Footer />
-
-<style lang="scss">
-	main {
-		margin: 2rem 1rem;
-
-		@include for(tablet-and-up) {
-			margin: 4rem auto 2rem;
-			width: 100%;
-			max-width: 46rem;
-		}
-	}
-</style>
+</Wireframe>

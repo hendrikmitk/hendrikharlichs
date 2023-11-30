e5f8a3fb31d1e01d214eb1fbff2c4910d5e99b8a
hendrikmitk <hendrikharlichs@fastmail.com>
2023-11-30 17:33:33 +0100

    feat: Add privacy disclaimer to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -64,6 +64,9 @@
 			rel="external noopener noreferrer">Source Code</a
 		>
 	</div>
+	<small class="footer__disclaimer">
+		No tracking. No cookies. No visit logs.
+	</small>
 </footer>
 
 <style lang="scss">
